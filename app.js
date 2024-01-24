d9e39807a415dcfc783cee62653d9b2f8b85045c
Basitakhter <154249701+Basitakhter@users.noreply.github.com>
2024-01-25 00:27:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+console.log 
+console.table(prompt)
+ 
+alert("Happy To Welcome You")
+ confirm("Do You Want To Continou?")
+ var inp = prompt("Enter the Number")
+  console.log(inp)
+ document.write(`${inp} x 1 = `, inp*1 + "<br>")
+ document.write(`${inp} x 2 = `, inp*2 + "<br>")
+ document.write(`${inp} x 3 = `, inp*3 + "<br>")
+ document.write(`${inp} x 4 = `, inp*4 + "<br>")
+ document.write(`${inp} x 5 = `, inp*5 + "<br>")
+ document.write(`${inp} x 6 = `, inp*6 + "<br>")
+ document.write(`${inp} x 7 = `, inp*7 + "<br>")
+ document.write(`${inp} x 8 = `, inp*8 + "<br>")
+ document.write(`${inp} x 9 = `, inp*9 + "<br>")
+ document.write(`${inp} x 10 = `, inp*10 + "<br>")
+
+ console.log
+
+ console.log(`${inp} x 1 = `, inp*1 + "<br>")
+ console.log(`${inp} x 2 = `, inp*2 + "<br>")
+ console.log(`${inp} x 3 = `, inp*3 + "<br>")
+ console.log(`${inp} x 4 = `, inp*4 + "<br>")
+ console.log(`${inp} x 5 = `, inp*5 + "<br>")
+ console.log(`${inp} x 6 = `, inp*6 + "<br>")
+ console.log(`${inp} x 7 = `, inp*7 + "<br>")
+ console.log(`${inp} x 8 = `, inp*8 + "<br>")
+ console.log(`${inp} x 9 = `, inp*9 + "<br>")
+ console.log(`${inp} x 10 = `, inp*10 + "<br>")
+
+
+
+
+
+const ptr = prompt("Tell me Your Percentage")
+
+if (ptr<= 40) {
+    console.log("Your are Fail")
+} else if (ptr<= 50) {
+    console.log("You Are Pass. But work Hard for batter Results")
+} else if(ptr<= 60){
+    console.log("Grade B")
+    console.log("Good You Are Pass. But work Hard for batter Results")
+}else if(ptr<= 70){
+    console.log("Grade A")
+    console.log("Great You Are Pass. But work Hard for batter Results")
+}else if(ptr<= 80){
+    console.log("Grade A+")
+    console.log("Excelent You Are Pass.")
+}else if(ptr<= 100){
+    console.log("Grade A++")
+    console.log("Excelent You Are Extra Ordinary.")
+
+}else{
+    console.log("Please Input the correct data")
+}
+
